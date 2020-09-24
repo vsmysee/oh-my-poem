67e172e2df8d6eb65c06c75301ce9049adeae141
vsmysee <vsmysee@gmail.com>
2020-09-24 21:09:08 +0800

    Update ZoomDialog.java

--- src/com/gui/ZoomDialog.java
+++ src/com/gui/ZoomDialog.java
@@ -19,6 +19,8 @@ public class ZoomDialog extends JDialog {
 
     private List<JPanel> poemLabels = new ArrayList<>();
 
+    private List<Point> points = new ArrayList<>();
+
     private Timer timer;
 
 
@@ -31,19 +33,35 @@ public class ZoomDialog extends JDialog {
 
         setDefaultCloseOperation(2);
 
-        ActionListener anAction = new ActionListener() {
+        ActionListener closeAction = new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 ZoomDialog.this.dispose();
             }
         };
-        getRootPane().registerKeyboardAction(anAction,
+        getRootPane().registerKeyboardAction(closeAction,
                 KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
                 JComponent.WHEN_IN_FOCUSED_WINDOW);
+
+
+        ActionListener restoreAction = new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                restoreScroll();
+            }
+        };
+        getRootPane().registerKeyboardAction(restoreAction,
+                KeyStroke.getKeyStroke(KeyEvent.VK_SPACE, 0),
+                JComponent.WHEN_IN_FOCUSED_WINDOW);
+
 
         poem = poem(poems);
         add(poem);
         pack();
+
+        if (getWidth() < 600) {
+            setSize(600,getHeight());
+        }
 
         resetPosition(poems, poem);
 
@@ -59,8 +77,13 @@ public class ZoomDialog extends JDialog {
 
         getRootPane().getInputMap().put(KeyStroke.getKeyStroke("RIGHT"),
                 "nextPoem");
+        getRootPane().getInputMap().put(KeyStroke.getKeyStroke("DOWN"),
+                "nextPoem");
 
         getRootPane().getInputMap().put(KeyStroke.getKeyStroke("LEFT"),
+                "lastPoem");
+
+        getRootPane().getInputMap().put(KeyStroke.getKeyStroke("UP"),
                 "lastPoem");
 
 
@@ -94,6 +117,26 @@ public class ZoomDialog extends JDialog {
 
     }
 
+    private void restoreScroll() {
+        for (int i = 0; i < poemLabels.size(); i++) {
+
+            Component component = poemLabels.get(i);
+            Point p = points.get(i);
+            if (p != null) {
+                p.setLocation(p);
+                component.setLocation(p);
+                component.repaint();
+            }
+
+        }
+    }
+
+
+    public void addScroll(JPanel panel) {
+        poemLabels.add(panel);
+    }
+
+
     private void resetPosition(List<String> poems, JComponent poem) {
         if (poems.size() > 10) {
             setLocation(ClockAndPoem.screenSize.width / 2 - (getWidth() / 2), (ClockAndPoem.screenSize.height - getHeight()) / 2);
@@ -108,13 +151,27 @@ public class ZoomDialog extends JDialog {
                     @Override
                     public void actionPerformed(ActionEvent e) {
 
-                        for (Component component : poemLabels) {
+                        for (int i = 0; i < poemLabels.size(); i++) {
+
+                            Component component = poemLabels.get(i);
+
+                            try {
+                                points.get(i);
+                            } catch (IndexOutOfBoundsException ex) {
+                                points.add(component.getLocation());
+                            }
 
                             int x = component.getLocation().x;
                             int y = component.getLocation().y;
 
                             component.setLocation(x, y - 5);
                             component.repaint();
+
+                            if (i == poemLabels.size() - 1 &&  y < 25) {
+
+                                restoreScroll();
+
+                            }
                         }
 
                     }
@@ -142,6 +199,10 @@ public class ZoomDialog extends JDialog {
         add(poem);
 
         pack();
+
+        if (getWidth() < 600) {
+            setSize(600,getHeight());
+        }
 
         resetPosition(poems, poem);
     }
@@ -242,7 +303,7 @@ public class ZoomDialog extends JDialog {
 
             index++;
 
-            poemLabels.add(panel);
+            addScroll(panel);
 
         }
 
@@ -251,15 +312,14 @@ public class ZoomDialog extends JDialog {
             poemContent.add(poemLabel);
         }
 
-        poemRoot.add(poemContent);
-
 
         JPanel bottom = new JPanel(new FlowLayout(FlowLayout.RIGHT));
         bottom.add(new ClockAndPoem.CirclePanel(poems.get(0), 20, 25));
-        poemRoot.add(bottom);
+        poemContent.add(bottom);
 
+        addScroll(bottom);
 
-        poemLabels.add(bottom);
+        poemRoot.add(poemContent);
 
         return poemRoot;
     }
