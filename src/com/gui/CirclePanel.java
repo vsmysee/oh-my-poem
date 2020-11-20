272a71bfab4e1138005cd9db1c87a4ab788db9fa
vsmysee <vsmysee@gmail.com>
2020-11-20 17:15:18 +0800

    Update CirclePanel.java

--- src/com/gui/CirclePanel.java
+++ src/com/gui/CirclePanel.java
@@ -8,20 +8,23 @@ public class CirclePanel extends JPanel {
 
     public CirclePanel(String tag) {
         setLayout(new GridLayout(0, 1));
-        this.setPreferredSize(new Dimension(14, 20));
         add(new JLabel("<html><font color='white'>" + tag + "</font></html>"));
     }
 
     public CirclePanel(String tag, int width, int height) {
         setLayout(new GridLayout(0, 1));
-        this.setPreferredSize(new Dimension(width, height));
         add(new JLabel("<html><font color='white' size=5>" + tag + "</font></html>"));
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.setColor(Color.RED);
+        Graphics2D g2d = (Graphics2D)g;
+        g2d.setColor(Color.RED);
+
+        g2d.setRenderingHint(
+                RenderingHints.KEY_ANTIALIASING,
+                RenderingHints.VALUE_ANTIALIAS_ON);
         g.fillRoundRect(0, 0, getWidth(), getHeight(), 5, 5);
     }
 }
